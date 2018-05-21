7479fb82f704bfe686c0180a6b2467d359c29743
Niccolla <nlosacco@ymail.com>
2018-05-21 11:16:57 +0200

    La mia app Django Girls, primo commit

--- django/django-code/blog/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Post #importa modello definito in model.py
-
-admin.site.register(Post) #Per far si che il nostro modello sia visibile nella pagina di admin
